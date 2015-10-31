47f282053de8563fce6445940ca9210581253bb2
Josh Panknin <jpanknin@gmail.com>
2015-10-31 16:14:13 -0400

    Created models/templates

--- project/movies/models.py
+++ project/movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+	name = models.CharField(max_length = 100)
+	note = models.TextField()
+	rate = models.IntegerField()
+
+	def __str__(self):
+		return self.name
+
+
+
+
+
+
+# always make class names in camel case
+# always have "Field" at the end of the field type
+# use the __str__ to make sure that the name prints instead
+#	of the object symbol
+# always test the model
